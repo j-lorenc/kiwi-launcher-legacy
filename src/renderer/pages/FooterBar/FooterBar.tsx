34116b32635838944f68bc109554a1110133028e
j-lorenc <jon@jonathanlorenc.com>
2020-07-04 12:23:31 -0500

    [SB-6] - Add refresh games list button to footer

--- src/renderer/pages/FooterBar/FooterBar.tsx
+++ src/renderer/pages/FooterBar/FooterBar.tsx
@@ -4,7 +4,7 @@ import LibraryIcon from '@fortawesome/fontawesome-pro/svgs/light/gamepad-alt.svg
 import StoreIcon from '@fortawesome/fontawesome-pro/svgs/light/store-alt.svg';
 import SyncIcon from '@fortawesome/fontawesome-pro/svgs/light/sync.svg';
 import SteamCredModal from '../../components/Modal/SteamCredModal';
-import { showSteamStore } from '../../events/game';
+import { showSteamStore, refreshGamesList } from '../../events/game';
 
 const FooterBar: React.FC = () => {
   const [showSteamCredModal, setShowSteamCredModal] = useState<boolean>(false);
@@ -12,20 +12,32 @@ const FooterBar: React.FC = () => {
   return (
     <>
       <footer>
-        <button
-          onClick={() => {
-            showSteamStore();
-          }}
-        >
-          <StoreIcon width="18" fill="white" />
-        </button>
-        <button
-          onClick={() => {
-            setShowSteamCredModal(!showSteamCredModal);
-          }}
-        >
-          <LibraryIcon width="20" fill="white" />
-        </button>
+        <div>
+          <button
+            title="Sync"
+            onClick={() => {
+              refreshGamesList();
+            }}
+          >
+            <SyncIcon width="12" fill="rgba(255,255,255,0.6)" />
+          </button>
+        </div>
+        <div>
+          <button
+            onClick={() => {
+              showSteamStore();
+            }}
+          >
+            <StoreIcon width="18" fill="rgba(255,255,255,0.6)" />
+          </button>
+          <button
+            onClick={() => {
+              setShowSteamCredModal(!showSteamCredModal);
+            }}
+          >
+            <LibraryIcon width="20" fill="rgba(255,255,255,0.6)" />
+          </button>
+        </div>
       </footer>
       <SteamCredModal active={showSteamCredModal} onClose={setShowSteamCredModal} />
     </>
